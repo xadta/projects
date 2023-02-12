d408f7c8768ffa5832fa96317fea02a7af8fe6f2
xadta <121993233+xadta@users.noreply.github.com>
2023-02-12 13:48:17 +1100

    Started implementing classes for fading transitions

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// Ensuring that the correct tab is highlighted on reload
+window.onload = () => {
+    if (document.URL.includes("index.html#profile-page")) {
+        updateClassList(document.getElementById("about-tab"), document.getElementById("projects-tab"));
+    } else if (document.URL.includes("index.html#projects-page")) {
+        updateClassList(document.getElementById("projects-tab"), document.getElementById("about-tab"));
+    }
+}
 
 // Changing the color of the nav buttons when clicked
 const updateClassList = (clickTab, removeTab) => {
@@ -17,11 +25,16 @@ const clickNavBar = (reqTabName, removeTabName) => {
 document.getElementById("projects-tab").onclick = () => clickNavBar("projects-tab", "about-tab");
 document.getElementById("about-tab").onclick = () => clickNavBar("about-tab", "projects-tab");
 
-// Ensuring that the correct tab is highlighted on reload
-window.onload = () => {
-    if (document.URL.includes("index.html#profile-page")) {
-        updateClassList(document.getElementById("about-tab"), document.getElementById("projects-tab"));
-    } else if (document.URL.includes("index.html#projects-page")) {
-        updateClassList(document.getElementById("projects-tab"), document.getElementById("about-tab"));
-    }
-}
+// Fade in transitions
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } else {
+            entry.target.classList.remove("show")
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll(".hide");
+hiddenElements.foreach((element) => observer.observe(element));
