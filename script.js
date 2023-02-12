806521358a6575eaa914560ddd24db98ff11905b
xadta <121993233+xadta@users.noreply.github.com>
2023-02-12 12:12:48 +1100

    Fixed tab behaviour on reload

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
+
 // Changing the color of the nav buttons when clicked
+const updateClassList = (clickTab, removeTab) => {
+    if (!clickTab.classList.contains("clicked")) clickTab.classList.add("clicked");
+    if (removeTab.classList.contains("clicked")) removeTab.classList.remove("clicked");
+}
+
 const clickNavBar = (reqTabName, removeTabName) => {
     // Get the tab we are clicking on and the tab we need to remove the click from
     const clickTab = document.getElementById(reqTabName); 
     const removeTab = document.getElementById(removeTabName);
 
     // Now, removing the clicked class from the removeTab and adding it to the clickTab
-    if (!clickTab.classList.contains("clicked")) clickTab.classList.add("clicked");
-    if (removeTab.classList.contains("clicked")) removeTab.classList.remove("clicked");
+    updateClassList(clickTab, removeTab);
 }
 
 document.getElementById("projects-tab").onclick = () => clickNavBar("projects-tab", "about-tab");
 document.getElementById("about-tab").onclick = () => clickNavBar("about-tab", "projects-tab");
 
 // Ensuring that the correct tab is highlighted on reload
-// const forceScroll = () => {
-//     clickNavBar("about-tab", "projects-tab");
-// }
-// window.onbeforeunload() = () => {
-//     document.querySelector('html').style.scrollBehavior = '';
-//     window.scrollTo(0);
-//     clickNavBar("about-tab", "projects-tab");
-// }
+window.onload = () => {
+    if (document.URL.includes("index.html#profile-page")) {
+        updateClassList(document.getElementById("about-tab"), document.getElementById("projects-tab"));
+    } else if (document.URL.includes("index.html#projects-page")) {
+        updateClassList(document.getElementById("projects-tab"), document.getElementById("about-tab"));
+    }
+}
